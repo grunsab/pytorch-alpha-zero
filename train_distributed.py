4c67c8e87e82cba44b88acfc531ebbbd9c63cde7
Rishi S <rishi@talkdoc.com>
2025-07-19 21:40:22 -0700

    Fix File Path Train Distributed

--- train_distributed.py
+++ train_distributed.py
@@ -16,7 +16,7 @@ import argparse
 num_epochs = 500
 num_blocks = 20
 num_filters = 256
-ccrl_dir = os.path.expanduser('~/ccrl/reformated')
+ccrl_dir = os.path.expanduser('ccrl/reformated')
 logmode = True
 
 def setup(rank, world_size, backend='nccl', init_method='env://'):
